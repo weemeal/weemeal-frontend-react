e44d5150fdea74f32fbbabf76ec826af4e662d63
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-09-04 13:00:24 +0200

    feat: redesign RecipeDetail

--- src/components/recipe/detail/RecipeDetail.tsx
+++ src/components/recipe/detail/RecipeDetail.tsx
@@ -4,9 +4,11 @@ import {deleteRecipe, fetchRecipeById} from '../../../Api';
 import QRCode from 'qrcode.react';
 import {marked} from 'marked';
 import './RecipeDetail.css';
+import {faPen, faTrash} from '@fortawesome/free-solid-svg-icons'; // Icons für Bearbeiten und Löschen
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const RecipeDetail: React.FC = () => {
-    const { id } = useParams<{ id: string }>();
+    const {id} = useParams<{ id: string }>();
     const [recipe, setRecipe] = useState<any | null>(null);
     const [loading, setLoading] = useState<boolean>(true); // Ladezustand
     const [error, setError] = useState<boolean>(false); // Fehlerzustand
@@ -30,7 +32,7 @@ const RecipeDetail: React.FC = () => {
         }
     }, [id]);
 
-  const handleDelete = async () => {
+    const handleDelete = async () => {
         if (id) {
             await deleteRecipe(id);
             window.location.href = '/';
@@ -51,32 +53,40 @@ const RecipeDetail: React.FC = () => {
 
     return (
         <div className="recipe-detail-container">
-            <div className="recipe-header">
+            <div className="recipe-actions">
                 <Link to={`/edit/${recipe.recipeId}`} className="edit-button">
-                    ✏️
+                    <FontAwesomeIcon icon={faPen}/> Bearbeiten
                 </Link>
-                <h1 className="recipe-name">{recipe.name}</h1>
-                <QRCode value={window.location.href} className="qr-code" />
+                <button onClick={handleDelete} className="delete-button">
+                    <FontAwesomeIcon icon={faTrash}/> Löschen
+                </button>
             </div>
-            <button onClick={handleDelete} className="delete-button">Löschen</button>
-            <p className="recipe-portions"><strong>Portionen:</strong> {recipe.recipeYield}</p>
-            <div className="recipe-content">
-                <div className="recipe-ingredients">
-                    <h2>Zutaten</h2>
-                    <div className="ingredients-list">
-                        {recipe.ingredients.map((ingredient: string, index: number) => (
-                            <div key={index} className="ingredient-card">
-                                {ingredient}
-                            </div>
-                        ))}
-                    </div>
+
+            <div className="recipe-card">
+                <div className="recipe-header">
+                    <h1 className="recipe-name">{recipe.name}</h1>
+                    <QRCode value={window.location.href} className="qr-code"/>
                 </div>
-                <div className="recipe-instructions">
-                    <h2>Zubereitung</h2>
-                    <div
-                        className="instructions-content"
-                        dangerouslySetInnerHTML={{ __html: marked(recipe.recipeInstructions) as string }}
-                    />
+                <hr className="divider"/>
+                <p className="recipe-portions"><strong>Portionen:</strong> {recipe.recipeYield}</p>
+                <div className="recipe-content">
+                    <div className="recipe-ingredients">
+                        <h2>Zutaten</h2>
+                        <div className="ingredients-list">
+                            {recipe.ingredients.map((ingredient: string, index: number) => (
+                                <div key={index} className="ingredient-card">
+                                    {ingredient}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="recipe-instructions">
+                        <h2>Zubereitung</h2>
+                        <div
+                            className="instructions-content"
+                            dangerouslySetInnerHTML={{__html: marked(recipe.recipeInstructions) as string}}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
